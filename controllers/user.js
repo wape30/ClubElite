17117627f589b6e7dc4a5b5b1ec8f3c02949f8d3
wape30 <wape30@gmail.com>
2018-07-06 17:57:54 -0500

    Avance #3
    Se crearon las carpetas /uploads/users y se agregaron los metodos de UploadImage en /controllers/user.js y /routes/user.js

--- controllers/user.js
+++ controllers/user.js
@@ -115,6 +115,40 @@ function updateUser(req, res){
   });
 }
 
+function uploadImage(req, res){
+  var userId = req.params.id;
+  var file_name = 'No subido...';
+
+  if(req.files){
+    var file_path = req.files.image.path;
+    var file_split = file_path.split('\\');
+    var file_name = file_split[2];
+
+    var ext_split = file_name.split('\.');
+    var file_ext = ext_split[1];
+
+    if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif'){
+      User.findByIdAndUpdate(userId, {img: file_name}, (err, userUpdated)=> {
+        if(err){
+          res.status(500).send({message: 'Error al actualizar el usuario'});
+        }else{
+          if(!userUpdated){
+           res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+         }else{
+           res.status(200).send({user: userUpdated});
+         }}
+        });
+    }else{
+      res.status(200).send({message: 'La extensión del archivo no es valida'});
+
+    }
+
+    console.log(file_path);
+  }else{
+    res.status(200).send({message: 'No has subido ninguna imagen...'});
+  }
+}
+
 
 
 /*para poder usar fuera del fichero los metodos*/
@@ -122,5 +156,6 @@ module.exports = {
   pruebas,
   saveUser,
   loginUser,
-  updateUser
+  updateUser,
+  uploadImage
 };
